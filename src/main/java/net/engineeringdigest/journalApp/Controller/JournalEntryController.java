c166b6ccd308a76d1a54cb8233c71f8f9d61ac12
Bhasin <bassanneeraj@gmail.com>
2026-06-17 22:49:18 +0530

    learnt crud operations

--- src/main/java/net/engineeringdigest/journalApp/Controller/JournalEntryController.java
+++ src/main/java/net/engineeringdigest/journalApp/Controller/JournalEntryController.java
@@ -1,7 +1,46 @@
 package net.engineeringdigest.journalApp.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/journal")
 public class JournalEntryController {
+
+    Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping("/get-entry")
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping("/post-entry")
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
+    @GetMapping("/get-entry/id/{id}")
+    public JournalEntry getEntryById(@PathVariable  long id){
+        if(journalEntries.containsKey(id)){
+            return journalEntries.get(id);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/delete-entry/{id}")
+    public void deleteEntry(@PathVariable long id){
+        journalEntries.remove(id);
+    }
+
+    @PutMapping("/update-entry/id/{id}")
+    public boolean updateEntryById(@PathVariable long id, @RequestBody JournalEntry myEntry){
+        journalEntries.put(id,myEntry);
+        return true;
+    }
 }
--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntity.java
+++
@@ -1,4 +0,0 @@
-package net.engineeringdigest.journalApp.entity;
-
-public class JournalEntity {
-}
